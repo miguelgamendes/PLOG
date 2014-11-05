33feec6a9ae2122c28e53139064ded6d778b3c99
zephiro309 <zephiro309@gmail.com>
2014-11-05 20:04:04 +0000

    Finished board printing functions.

--- project1/quantumleap.pl
+++ project1/quantumleap.pl
@@ -9,7 +9,7 @@ emptyBoard([[' ',' ',' ',' ',' ','#','#','#','#'],
 			['#','#','#','#',' ',' ',' ',' ',' ']
 			]).
 
-generateEmptyBoard() :-
+/*generateEmptyBoard() :-*/
 
 printBoard([]).
 printBoard([X1, X2, X3, X4, X5, X6, X7, X8, X9]) :- 
@@ -17,30 +17,41 @@ printBoard([X1, X2, X3, X4, X5, X6, X7, X8, X9]) :-
 	printLine2(X2),
 	printLine3(X3),
 	printLine4(X4),
-	printLine5(X5).
+	printLine5(X5),
+	printLine6(X6),
+	printLine7(X7),
+	printLine8(X8),
+	printLine9(X9).
+
+printLiteralLine([X|Xs]) :- write(X), printLiteralLine(Xs).
 
 printLine1([X|Xs]) :- write('    |'), write(X), printLine1aux(Xs).
-printLine1aux([]).
-printLine1aux([X|Xs]) :- X =\= '#', write(X), write('|'), printLine1aux(Xs).
+printLine1aux([]) :- write('|').
+printLine1aux([X|Xs]) :- X \== '#', write('|'), write(X), printLine1aux(Xs).
+printLine1aux([X|Xs]) :- X == '#', printLine1aux(Xs).
 
 printLine2([X|Xs]) :- write('   |'), write(X), printLine2aux(Xs).
-printLine2aux([]).
-printLine2aux([X|Xs]) :- X =\= '#', write(X), write('|'), printLine2aux(Xs).
+printLine2aux([]) :- write('|').
+printLine2aux([X|Xs]) :- X \== '#', write('|'), write(X), printLine2aux(Xs).
+printLine2aux([X|Xs]) :- X == '#', printLine2aux(Xs).
 
 printLine3([X|Xs]) :- write('  |'), write(X), printLine3aux(Xs).
-printLine3aux([]).
-printLine3aux([X|Xs]) :- X =\= '#', write(X), write('|'), printLine3aux(Xs).
+printLine3aux([]) :- write('|').
+printLine3aux([X|Xs]) :- X \== '#', write('|'), write(X), printLine3aux(Xs).
+printLine3aux([X|Xs]) :- X == '#', printLine3aux(Xs).
 
 printLine4([X|Xs]) :- write(' |'), write(X), printLine4aux(Xs).
-printLine4aux([]).
-printLine4aux([X|Xs]) :- X =\= '#', write(X), write('|'), printLine4aux(Xs).
+printLine4aux([]) :- write('|').
+printLine4aux([X|Xs]) :- X \== '#', write('|'), write(X), printLine4aux(Xs).
+printLine4aux([X|Xs]) :- X == '#', printLine4aux(Xs).
 
 printLine5([X|Xs]) :- write('|'), write(X), printLine5aux(Xs).
-printLine5aux([]).
-printLine5aux([X|Xs]) :- X =\= '#', write(X), write('|'), printLine5aux(Xs).
+printLine5aux([]) :- write('|').
+printLine5aux([X|Xs]) :- X \== '#', write('|'), write(X), printLine5aux(Xs).
+printLine5aux([X|Xs]) :- X == '#', printLine5aux(Xs).
 
-playGame() :-
-	#output
-	#input turno 1
-	#input turno 2
-	#playGame.
+/*playGame() :-
+	output
+	input turno 1
+	input turno 2
+	playGame.*/
