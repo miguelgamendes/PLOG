4b868f2fab3b40ec7163edf885342c9104069e3d
zephiro309 <zephiro309@gmail.com>
2014-11-11 15:55:24 +0000

    Completed random board generation.

--- project1/boardmanip.pl
+++ project1/boardmanip.pl
@@ -23,8 +23,7 @@ generateEmptyBoard(X) :-
 	append(X5, [L6], X6),
 	append(X6, [L7], X7),
 	append(X7, [L8], X8),
-	append(X8, [L9], X),
-	printBoard(X).
+	append(X8, [L9], X).
 
 generateEmptyBoardAux(L, N) :-
 	N == 1,
@@ -55,9 +54,76 @@ generateEmptyBoardAux(L, N) :-
 	append([],['#','#','#','#',' ',' ',' ',' ',' '],L).
 
 /* Game board generation */
-/*generateGameBoard(X) :-*/
-
+generateGameBoard(X) :-
+	genLine(1, L1),
+	genLine(2, L2),
+	genLine(3, L3),
+	genLine(4, L4),
+	genLine(5, L5),
+	genLine(6, L6),
+	genLine(7, L7),
+	genLine(8, L8),
+	genLine(9, L9),
+	append([], [L1], X1),
+	append(X1, [L2], X2),
+	append(X2, [L3], X3),
+	append(X3, [L4], X4),
+	append(X4, [L5], X5),
+	append(X5, [L6], X6),
+	append(X6, [L7], X7),
+	append(X7, [L8], X8),
+	append(X8, [L9], X).
 	
+genLine(N, Line) :-
+	N == 1,
+	genLineAux(0, 4, [], LAux),
+	append(LAux, ['#','#','#','#'], Line).
+genLine(N, Line) :-
+	N == 2,
+	genLineAux(0, 5, [], LAux),
+	append(LAux, ['#','#','#'], Line).
+genLine(N, Line) :-
+	N == 3,
+	genLineAux(0, 6, [], LAux),
+	append(LAux, ['#','#'], Line).
+genLine(N, Line) :-
+	N == 4,
+	genLineAux(0, 7, [], LAux),
+	append(LAux, ['#'], Line).
+genLine(N, Line) :-
+	N == 5,
+	genLineAux(0, 8, [], LAux),
+	append(LAux, [], Line).
+genLine(N, Line) :-
+	N == 6,
+	genLineAux(0, 7, [], LAux),
+	append(['#'], LAux, Line).
+genLine(N, Line) :-
+	N == 7,
+	genLineAux(0, 6, [], LAux),
+	append(['#','#'], LAux, Line).
+genLine(N, Line) :-
+	N == 8,
+	genLineAux(0, 5, [], LAux),
+	append(['#','#','#'], LAux, Line).
+genLine(N, Line) :-
+	N == 9,
+	genLineAux(0, 4, [], LAux),
+	append(['#','#','#','#'], LAux, Line).
+	
+genLineAux(N, FinalN, L1, L2) :- /* Generates line L2 with FinalN - 1 elements */
+	N == FinalN,
+	generateRandomPiece(NewPiece),
+	append(L1,[NewPiece],L2).
+
+genLineAux(N, FinalN, L1, L2) :-
+	N < FinalN,
+	N2 is N + 1,
+	generateRandomPiece(NewPiece),
+	append(L1, [NewPiece], LAux),
+	genLineAux(N2, FinalN, LAux, L2).
+
+/* Random piece generation */
 generateRandomPiece(X) :-
 	random(0, 2, N),
 	returnPiece(N, X).
@@ -116,3 +182,4 @@ changeLinePiece([L|Ls], N, X, Piece, L2) :-
 	N2 is N + 1,
 	changeLinePiece(Ls, N2, X, Piece, Laux),
 	append([L], Laux, L2).
+
