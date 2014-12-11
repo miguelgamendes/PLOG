6f8c1394d73247f4a8c246c17a1433de6df22792
zephiro309 <zephiro309@gmail.com>
2014-12-11 16:31:30 +0000

    Restructured project functions, added pseudo-UI goodies.

--- project2/driveyanuts.pl
+++ project2/driveyanuts.pl
@@ -1,13 +1,19 @@
 :- use_module(library(clpfd)).
 
-gamePiece([X1, X2, X3, X4, X5, X6]) :-
-	all_different([X1, X2, X3, X4, X5, X6]),
-	X1 #>= 1, X1 #=< 6,
-	X2 #>= 1, X2 #=< 6,
-	X3 #>= 1, X3 #=< 6,
-	X4 #>= 1, X4 #=< 6,
-	X5 #>= 1, X5 #=< 6,
-	X6 #>= 1, X6 #=< 6.
+:- include('logic.pl').
+
+:- 	write('****************************\n'),
+	write('* start. - Starts the game *\n'),
+	write('*                          *\n'),
+	write('*         WARNING:         *\n'),
+	write('*     May contain nuts     *\n'),
+	write('****************************\n').
+
+start :-
+	write('ATTEMPTING TO DRIVE YA NUTS'), /* isto permite comecar um jogo sem chamar clausula */
+	/* game will be called here */
+	write('NUTS HAVE BEEN DRIVEN').
+
 /*
 driveyanuts() :-
 	.*/
