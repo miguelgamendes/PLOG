6c1ea6b66e750037454a41dcef3c25f624e14daa
zephiro309 <zephiro309@gmail.com>
2014-12-11 15:58:59 +0000

    Added output file, completed piece verification.

---
+++ project2/driveyanuts.pl
@@ -0,0 +1,13 @@
+:- use_module(library(clpfd)).
+
+gamePiece([X1, X2, X3, X4, X5, X6]) :-
+	all_different([X1, X2, X3, X4, X5, X6]),
+	X1 >= 1, X1 =< 6,
+	X2 >= 1, X2 =< 6,
+	X3 >= 1, X3 =< 6,
+	X4 >= 1, X4 =< 6,
+	X5 >= 1, X5 =< 6,
+	X6 >= 1, X6 =< 6.
+/*
+driveyanuts() :-
+	.*/
