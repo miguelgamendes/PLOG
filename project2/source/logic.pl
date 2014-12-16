2a6aabfbb50845a51f3010cfde3a5f927dc22737
zephiro309 <zephiro309@gmail.com>
2014-12-16 16:48:40 +0000

    Program is spitting out solutions instantaneously. Correctness needs to be confirmed.

--- project2/source/logic.pl
+++ project2/source/logic.pl
@@ -29,7 +29,12 @@ generateStandardPieces(Board, FinalBoard) :-
 
 generateTestPieces(Board, FinalBoard) :-
 	append(Board, [3, 1, 6, 2, 4, 5, 3, 1, 6, 2, 4, 5], Board1),
-	append([Board1], [[2, 3, 5, 1, 4, 6, 2, 3, 5, 1, 4, 6]], FinalBoard).
+	append([Board1], [[2, 3, 5, 1, 4, 6, 2, 3, 5, 1, 4, 6]], Board2),
+	append(Board2, [[3, 2, 4, 1, 6, 5, 3, 2, 4, 1, 6, 5]], Board3),
+	append(Board3, [[1, 4, 3, 6, 5, 2, 1, 4, 3, 6, 5, 2]], Board4),
+	append(Board4, [[4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3]], Board5),
+	append(Board5, [[2, 5, 3, 1, 6, 4, 2, 5, 3, 1, 6, 4]], Board6),
+	append(Board6, [[5, 4, 3, 2, 1, 6, 5, 4, 3, 2, 1, 6]], FinalBoard).
 
 generateBoard('standard', Board, FinalBoard) :-
 	generateTestPieces(Board, FinalBoard).
@@ -199,11 +204,16 @@ solveNuts2(Board, 'standard') :-
 	write('Top Left Piece Rotation: '), write(R6), write('\n').
 
 solveTestNuts(Board, 'standard') :-
-	Rotations = [R0, R1],
+	Rotations = [R0, R1, R2, R3, R4, R5, R6],
 	domain(Rotations, 0, 5),
 	/* get pieces */
 	getStandardBoardPiece(Board, 0, MiddlePiece),
 	getStandardBoardPiece(Board, 1, TopPiece),
+	getStandardBoardPiece(Board, 2, TopRightPiece),
+	getStandardBoardPiece(Board, 3, BottomRightPiece),
+	getStandardBoardPiece(Board, 4, BottomPiece),
+	getStandardBoardPiece(Board, 5, BottomLeftPiece),
+	getStandardBoardPiece(Board, 6, TopLeftPiece),
 	/* apply rotation conditions */
 	RotatedMiddle1 #= 1 + R0,
 	RotatedMiddle2 #= 2 + R0,
@@ -217,7 +227,36 @@ solveTestNuts(Board, 'standard') :-
 	RotatedTop4 #= 4 + R1,
 	RotatedTop5 #= 5 + R1,
 	RotatedTop6 #= 6 + R1,
-	RotatedTopRight1
+	RotatedTopRight1 #= 1 + R2,
+	RotatedTopRight2 #= 2 + R2,
+	RotatedTopRight3 #= 3 + R2,
+	RotatedTopRight4 #= 4 + R2,
+	RotatedTopRight5 #= 5 + R2,
+	RotatedTopRight6 #= 6 + R2,
+	RotatedBottomRight1 #= 1 + R3,
+	RotatedBottomRight2 #= 2 + R3,
+	RotatedBottomRight3 #= 3 + R3,
+	RotatedBottomRight4 #= 4 + R3,
+	RotatedBottomRight5 #= 5 + R3,
+	RotatedBottomRight6 #= 6 + R3,
+	RotatedBottom1 #= 1 + R4,
+	RotatedBottom2 #= 2 + R4,
+	RotatedBottom3 #= 3 + R4,
+	RotatedBottom4 #= 4 + R4,
+	RotatedBottom5 #= 5 + R4,
+	RotatedBottom6 #= 6 + R4,
+	RotatedBottomLeft1 #= 1 + R5,
+	RotatedBottomLeft2 #= 2 + R5,
+	RotatedBottomLeft3 #= 3 + R5,
+	RotatedBottomLeft4 #= 4 + R5,
+	RotatedBottomLeft5 #= 5 + R5,
+	RotatedBottomLeft6 #= 6 + R5,
+	RotateTopLeft1 #= 1 + R6,
+	RotateTopLeft2 #= 2 + R6,
+	RotateTopLeft3 #= 3 + R6,
+	RotateTopLeft4 #= 4 + R6,
+	RotateTopLeft5 #= 5 + R6,
+	RotateTopLeft6 #= 6 + R6,
 	/* get elements, with rotations */
 	element(RotatedMiddle1, MiddlePiece, MiddleElement1),
 	element(RotatedMiddle2, MiddlePiece, MiddleElement2),
@@ -231,14 +270,59 @@ solveTestNuts(Board, 'standard') :-
 	element(RotatedTop4, TopPiece, TopElement4),
 	element(RotatedTop5, TopPiece, TopElement5),
 	element(RotatedTop6, TopPiece, TopElement6),
-	element()
+	element(RotatedTopRight1, TopRightPiece, TopRightElement1),
+	element(RotatedTopRight2, TopRightPiece, TopRightElement2),
+	element(RotatedTopRight3, TopRightPiece, TopRightElement3),
+	element(RotatedTopRight4, TopRightPiece, TopRightElement4),
+	element(RotatedTopRight5, TopRightPiece, TopRightElement5),
+	element(RotatedTopRight6, TopRightPiece, TopRightElement6),
+	element(RotatedBottomRight1, BottomRightPiece, BottomRightElement1),
+	element(RotatedBottomRight2, BottomRightPiece, BottomRightElement2),
+	element(RotatedBottomRight3, BottomRightPiece, BottomRightElement3),
+	element(RotatedBottomRight4, BottomRightPiece, BottomRightElement4),
+	element(RotatedBottomRight5, BottomRightPiece, BottomRightElement5),
+	element(RotatedBottomRight6, BottomRightPiece, BottomRightElement6),
+	element(RotatedBottom1, BottomPiece, BottomElement1),
+	element(RotatedBottom2, BottomPiece, BottomElement2),
+	element(RotatedBottom3, BottomPiece, BottomElement3),
+	element(RotatedBottom4, BottomPiece, BottomElement4),
+	element(RotatedBottom5, BottomPiece, BottomElement5),
+	element(RotatedBottom6, BottomPiece, BottomElement6),
+	element(RotatedBottomLeft1, BottomLeftPiece, BottomLeftElement1),
+	element(RotatedBottomLeft2, BottomLeftPiece, BottomLeftElement2),
+	element(RotatedBottomLeft3, BottomLeftPiece, BottomLeftElement3),
+	element(RotatedBottomLeft4, BottomLeftPiece, BottomLeftElement4),
+	element(RotatedBottomLeft5, BottomLeftPiece, BottomLeftElement5),
+	element(RotatedBottomLeft6, BottomLeftPiece, BottomLeftElement6),
+	element(RotatedTopLeft1, TopLeftPiece, TopLeftElement1),
+	element(RotatedTopLeft2, TopLeftPiece, TopLeftElement2),
+	element(RotatedTopLeft3, TopLeftPiece, TopLeftElement3),
+	element(RotatedTopLeft4, TopLeftPiece, TopLeftElement4),
+	element(RotatedTopLeft5, TopLeftPiece, TopLeftElement5),
+	element(RotatedTopLeft6, TopLeftPiece, TopLeftElement6),
 	/* be a nice dude, and constrain stuff */
-	MiddleElement1 #= TopElement4,
+	MiddleElement1 #= TopElement1,
+	MiddleElement2 #= TopRightElement5,
+	MiddleElement3 #= BottomRightElement6,
+	MiddleElement4 #= BottomElement1,
+	MiddleElement5 #= BottomLeftElement2,
+	MiddleElement6 #= TopLeftElement3,
+	TopElement3 #= TopRightElement6,
+	TopRightElement4 #= BottomRightElement1,
+	BottomRightElement5 #= BottomElement2,
+	BottomElement6 #= BottomLeftElement3,
+	BottomLeftElement1 #= TopLeftElement4,
+	TopLeftElement2 #= TopElement5,
 	write('Processing solutions...\n'),
 	/* process possible solutions */
 	labeling([], Rotations),
 	write('Middle Rotation: '), write(R0), write('\n'),
-	write('Top Piece Rotation: '), write(R1), write('\n').
+	write('Top Piece Rotation: '), write(R1), write('\n'),
+	write('Top Right Piece Rotation: '), write(R2), write('\n'),
+	write('Bottom Right Piece Rotation: '), write(R3), write('\n'),
+	write('Bottom Piece Rotation: '), write(R4), write('\n'),
+	write('Bottom Left Piece Rotation: '), write(R5), write('\n'),
+	write('Top Left Piece Rotation: '), write(R6), write('\n').
 
 solveNuts(Board, 'dynamic').
 
